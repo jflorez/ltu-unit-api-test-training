d7a76ea2475addd6713361a99e6445c2417db78b
Juan Florez <jflorez@planittesting.com>
2024-09-15 18:51:28 +1000

    update exercise comments

--- src/test/java/com/planit/simpleREST/InterestUnitTests.java
+++ src/test/java/com/planit/simpleREST/InterestUnitTests.java
@@ -11,11 +11,6 @@ import com.planit.simpleREST.loans.LoanEntry;
 
 public class InterestUnitTests {
 
-    /**
-     * TODO: This test attempts to test a non-testable method.
-     * Why is the test not a good unit test?
-     * What makes the method non-testable?
-     */
     @Test void checkSimpleInterest() {
         var loan = new LoanEntry("UNITID1", LocalDate.now(), 1000, 2, 2.6);
         var interest = new InterestCalculator().simpleInterest(loan);
